871b4da86240464c272d2ad3bb0f213d0c8c28f7
jerry20201230 <jerry20200815@gmail.com>
2024-04-07 14:58:05 +0800

    test endpoint

--- server.js
+++ server.js
@@ -6,24 +6,124 @@ const dayjs = require('dayjs')
 
 app.use(bodyParser.json());
 app.use(express.static('./build'));
-
+var cron = require('node-cron');
 // app.use(express.json());
 
-app.get(/^\/api\/get/, (req, res) => {
-    console.log(req.path + req.params)
-    fetch(req.path.replace("/api/get/", ""), {
-        method: "GET",
+var sql_Connect = mysql.createPool({
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  port: process.env.MYSQLPORT,
+  database: process.env.MYSQLDATABASE,
+
+  // 無可用連線時是否等待pool連線釋放(預設為true)
+  waitForConnections: true,
+  // 連線池可建立的總連線數上限(預設最多為10個連線數)
+  connectionLimit: 15
+});
+
+async function getapikey(callback) {
+  var tdxLogin = {
+    grant_type: "client_credentials",
+    client_id: "jerry20200815-905e4c2d-f4f9-42dd",
+    client_secret: "df5c085e-f262-4258-b1d6-518e40138f71"
+  };
+  await fetch("https://tdx.transportdata.tw/auth/realms/TDXConnect/protocol/openid-connect/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: new URLSearchParams(tdxLogin),
+  })
+    .then(response => response.json())
+    .then(data => {
+      localStorage.setItem("loginAccess", JSON.stringify(data))
+      callback(true)
+    })
+    .catch(error => {
+      console.error("Error:", error);
+    });
+
+}
+
+app.post(/^\/api\/get/, (req, res) => {
+  var apikey =
+    fetch(req.apiurl, {
+      method: "GET",
+      headers: {
+        "authorization": "Bearer " + accesstoken.access_token,
+      }
 
     }).then(r => r.json())
-        .then(res.send(r))
+      .then(res.send(r))
 })
 
 app.get('*', (req, res) => {
-    res.sendFile('index.html', { root: './build' });
+  res.sendFile('index.html', { root: './build' });
+})
+
+
+function getApiKeyFromDB() {
+  var temp = ""
+  sql_Connect.getConnection(function (err, connection) {
+    connection.query(`
+      SELECT * FROM APIkey
+    `, function (error, results, field) {
+      if (error) console.log(error); connection.release()
+      console.log(results[0])
+      temp = results[0]
+      return temp
+    })
+  })
+  return temp
+}
+
+
+
+var getApiKey = cron.schedule('10 * * * *', () => {
+  var tdxLogin = {
+    grant_type: "client_credentials",
+    client_id: "jerry20200815-905e4c2d-f4f9-42dd",
+    client_secret: "df5c085e-f262-4258-b1d6-518e40138f71"
+  };
+  fetch("https://tdx.transportdata.tw/auth/realms/TDXConnect/protocol/openid-connect/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: new URLSearchParams(tdxLogin),
+  })
+    .then(response => response.json())
+    .then(data => {
+
+      sql_Connect.getConnection(function (err, connection) {
+        connection.query(`
+            DELETE FROM APIkey
+             WHERE id > 1;
+             INSERT INTO APIkey (apiKey)
+             VALUES(?);
+                `[JSON.stringify(data)], function (error, results, fields) {
+          if (error) {
+            console.error("Error:", error);
+            connection.release()
+            return
+          }
+          else {
+            console.log("API KEY 取得成功")
+          }
+          connection.release()
+        })
+      })
+
+    })
+    .catch(error => {
+      console.error("Error:", error);
+    });
 })
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  getApiKey.start()
+  console.log(`Server is running on port ${PORT}`);
 });
 
