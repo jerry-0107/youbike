21f1bf761353f7f6424beef561b24fabd2e3f492
jerry20201230 <jerry20200815@gmail.com>
2024-04-21 14:54:18 +0800

    fix bugs

--- src/favoriteBtn.jsx
+++ src/favoriteBtn.jsx
@@ -4,23 +4,60 @@ import FavoriteBorder from '@mui/icons-material/FavoriteBorder';
 import Favorite from '@mui/icons-material/Favorite';
 import { pink } from '@mui/material/colors';
 
-function editFavoriteList() {
+function editFavoriteList(data, method) {
+    var favoriteStations = JSON.parse(localStorage.getItem("favoriteStations"))
+    if (favoriteStations) {
 
+    } else {
+        localStorage.setItem("favoriteStations", "[]")
+        favoriteStations = JSON.parse(localStorage.getItem("favoriteStations"))
+    }
+
+    if (method === "add") {
+        favoriteStations.push(data)
+        localStorage.setItem(JSON.stringify(favoriteStations))
+    }
+    else if (method === "remove") {
+        var newArr = favoriteStations.filter((v) => { return v.uid !== data.uid })
+        localStorage.setItem(JSON.stringify(newArr))
+    }
 }
 
-const label = { inputProps: { 'aria-label': 'Checkbox demo', onchange: (e) => { if (e.target.checked) { }; console.log(e) } } };
+
+function recordRecentData(data) {
+    var recentData = JSON.parse(localStorage.getItem("recentData"))
+    if (recentData) {
+        if (recentData.length > 99) {
+            recentData.reverse().pop()
+            recentData.reverse()
+        }
+        var oldData = (recentData)
+        oldData.push(data)
+        localStorage.setItem("recentData", JSON.stringify(oldData))
+
+    } else {
+        var newData = JSON.stringify([data])
+        localStorage.setItem("recentData", (newData))
+    }
+}
+
+
 
 
 export default function FavoriteBtn({ stationName, stationUID, options }) {
-    return (
-        <div>
-            <Checkbox {...label} icon={<FavoriteBorder />} checkedIcon={<Favorite />} sx={{
-                color: pink[800],
-                '&.Mui-checked': {
-                    color: pink[600],
-                },
+    const label = { inputProps: { 'aria-label': 'Checkbox demo', onchange: (e) => { if (e.target.checked) { editFavoriteList({ name: stationName, uid: stationUID }, "add") }; console.log(e) } } };
 
-            }} />
-        </div>
+
+    return (
+
+        <Checkbox {...label} icon={<FavoriteBorder />} checkedIcon={<Favorite />} sx={{
+            color: pink[800],
+            '&.Mui-checked': {
+                color: pink[600],
+            },
+            display: "inline-block"
+
+        }} />
+
     );
 }
